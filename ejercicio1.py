c79cdd4d85bced0f0888d406da2dd4167add6a29
Santiago Rossetti Iñiguez <130598026+santiagorossetti@users.noreply.github.com>
2024-09-26 11:31:29 -0300

    ejercicio 1 del curso

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+Nombre = input("ingrese su nombre: ")
+Edad = int(input("Ahora, ingrese su edad: "))
+Altura = float(input("Por ultimo, ingrese su altura: "))
+
+print(f"Bienvenido {Nombre}")
+print(f"Acutalmente tienes {Edad} años, hace 5 años tenias {Edad - 5} y dentro de 2 años tendras {Edad + 2}")
