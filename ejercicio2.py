4ff46cb264fa2b7d3d0abfe96c1cbe422ee4be14
Santiago Rossetti Iñiguez <130598026+santiagorossetti@users.noreply.github.com>
2024-09-27 18:04:07 -0300

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+## Crear un programa que solicite la edad del usuario y muestre por consola si el usuario es  mayor de edad o no.
+
+print("bienvenido usuario")
+Edad = int(input("ingrese su edad y le dire si es mayor de edad o no: "))
+
+if Edad >= 18:
+  print("Felicidades, eres mayor de edad")
+else:
+  print("eres menor de edad")
